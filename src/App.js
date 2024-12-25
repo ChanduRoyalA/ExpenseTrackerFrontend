571cfef51989ac7f6d40f5f7614e203e2f3d20f3
ChanduRoyalA <chandua.royal@gmail.com>
2024-12-25 20:18:24 +0530

    update screen

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ import Visuals from './components/Visuals.js';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path='/register' element={<Register />} />
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/addExpense" element={<ExpenseItem />} />
-        <Route exact path='/visuals' element={<Visuals />} />
-      </Routes>
+      <div className=' hidden md:grid'>
+        <Routes>
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/addExpense" element={<ExpenseItem />} />
+          <Route exact path='/visuals' element={<Visuals />} />
+        </Routes>
+      </div>
+      <div className='flex flex-col text-center justify-center h-screen md:hidden'>
+        <h1 className='text-xl text-red-500 font-bold'>NOT SUPPORTED FOR SMALL SCREENS. OPEN IN MEDIUM AND LARGE SCREEN FOR CLEAR VISUALS</h1>
+      </div>
     </Router>
   );
 }
